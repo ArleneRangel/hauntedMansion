6527231411cec2c0a8a144061008ccdb55ebc25f
ArleneRangel <arlenemrangel@gmail.com>
2022-06-07 21:52:59 -0700

    remove URL + deleted comments + corrected  spelling

--- hauntedMansion/urls.py
+++ hauntedMansion/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hauntedmansiontwo', views.hauntedmansiontwo, name='hauntedmansiontwo'),
+    path('', views.hauntedmansiontwo, name='hauntedmansiontwo'),
 ]
